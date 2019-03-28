c4c09f908cbd6fd5dfdd8f85bc367d5d0f436365
谢松青 <1043423813@qq.com>
2019-03-28 18:35:16 +0800

    修改

--- page/project/issue2/index.js
+++ page/project/issue2/index.js
@@ -82,8 +82,8 @@ Page({
         title: "",
         role: "",
         members: [],
-        startTime: "",
-        endTime: "",
+        startTime: "请选择",
+        endTime: "请选择",
         startDay: 0,
         endDay: 0,
         startPerformance: 0,
@@ -93,10 +93,14 @@ Page({
     },
     tempRole: "",
     roleIndex: 0,
+    memberIndex: 0,
+    timeIndex: 0,
     tempTag: "",
+    membersPicker: false,
     rolesPicker: false,
     timePicker: false,
-    tiemName: "start",
+    timeName: "start",
+    tempMember: {},
     selectedLables: '已解决',
     tags: [
         {
@@ -132,7 +136,13 @@ Page({
         name: "王五",
         job: "设计"
       }
-    ]
+    ],
+    yearIndex: 0,
+    monthIndex: 0,
+    dayIndex: 0,
+    years: ["2012", "2013", "2014", "2015", "2016"],
+    months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
   },
 
   onTagChange1(e) {
@@ -261,10 +271,96 @@ Page({
   },
   saveRoleChange(e) {
     let temp = this.data.tempRole;
-    alert(temp);
      this.setData({
         rolesPicker: false,
         "dropdownSelectData.sortData.role": temp
+    });
+  },
+  addMember(e) {
+    let temp = this.data.membersPicker;
+    this.setData({
+      membersPicker: !temp
+    });
+  },
+  memberChange(e) {
+    let index = e.detail.value;
+    let temp = this.data.members[index];
+    this.setData({
+      tempMember: temp
+    });
+  },
+  deleteMember(e) {
+    let index = e.target.dataset.index;
+    let members = this.data.dropdownSelectData.sortData.members;
+    alert(typeof members);
+  },
+  saveMemberChange(e) {
+    let temp = this.data.tempMember;
+    let length = this.data.dropdownSelectData.sortData.members.length;
+    this.data.dropdownSelectData.sortData.members = this.data.dropdownSelectData.sortData.members.push(temp);
+    this.setData({
+      membersPicker: false
+    });  
+  },
+  setTime(e) {
+    let temp = this.data.timePicker;
+    let name = e.target.dataset.name;
+    this.setData({
+      timePicker: !temp,
+      timeName: name
+    });
+  },
+  timeChange(e) {
+    let value = e.detail.value;    
+    let yearIndex = value[0];
+    let monthIndex = value[1];
+    let dayIndex = value[2];
+    let days = this.data.days;
+
+    if(monthIndex == 1) {
+      days = days.filter(function(n) {
+        return n < 29;
+      });
+    } else if (monthIndex == 3 || monthIndex == 5 || monthIndex == 8 || monthIndex == 10) {
+      days = days.filter(function(n) {
+        return n < 31;
+      });
+    }
+
+    this.setData({
+      yearIndex: yearIndex,
+      monthIndex: monthIndex,
+      dayIndex: dayIndex,
+      days: days
+    });
+  },
+  saveTimeChange(e) {
+    let data = this.data.dropdownSelectData;
+    let year = this.data.years[this.data.yearIndex];
+    let month = this.data.months[this.data.monthIndex];
+    let day = this.data.days[this.data.dayIndex];
+    let date = year + '-' + month + '-' + day;
+
+    let startDay = data.sortData.startTime;
+    let endDay = data.sortData.endTime;
+
+    if(this.data.timeName === "start"){
+      startDay = date;
+    } else {
+      endDay = date;
+    }
+
+     this.setData({
+        timePicker: false,
+        "dropdownSelectData.sortData.startTime": startDay,
+        "dropdownSelectData.sortData.endTime": endDay
+    });
+  },
+  cancelChange(e) {
+    this.setData({
+      timePicker: false,
+      rolesPicker: false,
+      membersPicker: false
     });
   }
 });
